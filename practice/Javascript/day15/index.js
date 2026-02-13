f99b33923e7309e11da56b521c0c5a8c1913fea2
sagar <imsagar4506@gmail.com>
2026-02-13 13:22:52 +0530

    implement DOM manipulation for task input and list creation

--- practice/Javascript/day15/index.js
+++ practice/Javascript/day15/index.js
@@ -30,8 +30,8 @@ let isBananaPresent = fruits.includes("Banana");
 let fruits1 = ["apple", "banana", "orange", "grape", "mango"];
 
 let results = fruits1.slice(1, 4);
-// console.log(results);
-// console.log(fruits1);
+console.log(results);
+console.log(fruits1);
 
 let profileName = "SagarSrinivas";
 
@@ -39,7 +39,81 @@ const Letter = profileName.slice(0, 5);
 console.log(Letter);
 
 // splice
-fruits1.splice(2, 2, "kiwi", "melon");
+fruits1.splice(3, 0, "kiwi");
 console.log(fruits1);
 
+// forEach method
 
+const result = fruits1.forEach(() => {
+  console.log(fruits1);
+});
+
+console.log(result);
+
+let arrNum = [1, 2, 3, 4, 5];
+
+// find method
+let arrNum1 = [1, 2, 3, 4, 5];
+
+const result2 = arrNum1.find((num) => num > 3);
+console.log(result2);
+
+// filter method
+const result3 = arrNum1.filter((num) => num > 3);
+console.log(result3);
+
+const result1 = arrNum.forEach((num) => console.log(num * 2));
+console.log(result1);
+
+// map method
+const result4 = arrNum1.map((num) => num * 2);
+console.log(result4);
+
+let cartvalues = [100, 200, 300, 400];
+console.log(cartvalues.length);
+
+// reduce method
+const result5 = cartvalues.reduce((acc, num) => acc + num, 0);
+console.log(result5);
+
+// some method
+const result6 = cartvalues.some((num) => num > 200);
+console.log(result6);
+
+// every method
+const result7 = cartvalues.every((num) => num > 50);
+console.log(result7);
+
+const elements = ["Fire", "Air", "Water"];
+
+console.log(elements.join());
+// Expected output: "Fire,Air,Water"
+
+console.log(elements.join(""));
+// Expected output: "FireAirWater"
+
+console.log(elements.join(" "));
+// Expected output: "Fire-Air-Water"
+
+// split method
+const str = "He ll ow or ld";
+const words = str.split(" ");
+console.log(words);
+
+let obj = {
+  name: "Sagar",
+  age: 25,
+  city: "Bangalore",
+};
+console.log(Object.keys(obj));
+console.log(Object.values(obj));
+console.log(Object.entries(obj));
+
+console.log(obj.name);
+console.log(obj["name"]);
+
+let arrPractice = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let sum = arrPractice.filter((num) => num < 5);
+console.log(sum);
+
+//
